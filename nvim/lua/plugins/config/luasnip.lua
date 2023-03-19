e3b75d5e2be6119d575dd11d573a559d915403e3
krapjost <krapjost@gmail.com>
2023-03-19 13:13:04 +0900

    mv snippets

--- nvim/lua/plugins/config/luasnip.lua
+++ nvim/lua/plugins/config/luasnip.lua
@@ -1,4 +1,4 @@
 return function()
   local snipmate = require('luasnip.loaders.from_snipmate')
-  snipmate.lazy_load({ paths = '~/.config/nvim/snippets' })
+  snipmate.lazy_load({ paths = '~/snippets/snipmate/snippets' })
 end
